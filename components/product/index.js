a4ea63b9c91bbac443840565f95ed38f5d8e0109
bair Gatpov <bairgatapov93@mail.ru>
2025-03-23 20:21:35 +0300

    Исправление

--- components/product/index.js
+++ components/product/index.js
@@ -7,10 +7,17 @@ export class ProductComponent {
     getHTML(data) {
         return (
             `
-             <h5 class="card-title">${data.title}</h5>
-                <p class="card-text">${data.text}</p>
-                    <img src="${data.src}" class="img" alt="картинка">   
-                
+            <div class="row">
+                <div class="product">
+                    <div class="card" h-100 shadow-sm data-id="${data.id}">
+                        <img src="${data.src}" class="img" alt="${data.title}">
+                        <div class="card-body">
+                            <h5 class="card-title">${data.title}</h5>
+                            <p class="card-text">${data.text}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>       
             `
         )
     }
